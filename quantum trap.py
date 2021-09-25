35f796f82dada8838022240c3e88806a1781a449
Alban Degezelle <88600841+AlbanDeg@users.noreply.github.com>
2021-09-26 01:50:56 +0200

    Add files via upload

---
+++ quantum trap.py
@@ -0,0 +1,84 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import math
+import seaborn as sns; sns.set_theme()
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+
+one_D='on'
+two_D='off'
+#################################################################################################
+## 1D
+#paramater
+if one_D=='on':
+    y=[]
+    x=[]
+    t=[]
+    L=10
+    dom=np.linspace(-L/2,L/2,300)
+    for point in dom:
+        x.append(point)
+    Norm=1*math.sqrt(2/L)
+    N=[1,2,3,4,5,6,7,8,9,10]
+    def E_n(n):
+        return 1*(n**2)/L**2
+    colors=['r','b','g','c','m','y','k','lime','magenta','sienna']
+    #ploting
+    plt.figure(figsize=(7,18.5))
+    plt.xlabel('Position')
+    plt.ylabel('Well')
+
+    plt.legend()
+    def phi_impair(x,n):
+        return 0.4*Norm*math.cos(n*math.pi/L*x)+E_n(n)
+    def phi_pair(x,n):
+        return 0.4*Norm*math.sin(n*math.pi/L*x)+E_n(n)
+
+    all_n=[]
+
+    for n in N:
+        if n%2==0:
+            y=[]
+            t=[]
+            for i in x:
+                y.append(phi_pair(i,n))
+            plt.title(f'Infinite Quantum well N={n}')
+            plt.plot(x,y,color=colors[n-1],label=f'n={n}')
+            plt.legend()
+
+        elif n%2!=0:
+            y=[]
+            t=[]
+            for i in x:
+                y.append(phi_impair(i,n))
+            plt.title(f'Infinite Quantum well N={n}')
+            plt.plot(x,y,color=colors[n-1],label=f'n={n}')
+
+
+    plt.show()
+
+##############################################################################################################
+## 2D ### TEST ZONE###
+if two_D=='on':
+    L_x=10
+    L_y=10
+
+    n_x=1
+    n_y=6
+    x=np.arange(-L_x/2,L_x/2,0.1)
+    y=np.arange(-L_y/2,L_y/2,0.1)
+    z_grid=np.zeros(shape=(100,100))
+    def prob(x,y):
+        return (math.sqrt(2/L_x)*math.sin(n_x*math.pi*x/L_x)*math.sqrt(2/L_y)*math.sin(n_y*math.pi*y/L_y))**2
+    for i in range(len(x)):
+        for j in range(len(y)):
+            z_grid[i][j]=prob(x[i],y[j])
+    plt.figure()
+    plt.xlabel('X postion')
+    plt.ylabel('Y position')
+    plt.title(f'2D Quantum Well : Porbability of presence nx={n_x} ; ny={n_y}')
+    sns.heatmap(data=z_grid,yticklabels=False,xticklabels=False,cmap="bwr")
+    plt.show()
+
+
